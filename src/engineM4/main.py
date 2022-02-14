f76c688aa8befd91dff16c872e6f49c0104d9eb9
NP <76001641+CoderN-P@users.noreply.github.com>
2022-02-14 13:52:41 -0800

    Fixed a few bugs

--- src/engineM4/main.py
+++ src/engineM4/main.py
@@ -225,22 +225,22 @@ def make_font(fonts, size) -> pygame.font.Font | pygame.font.SysFont:
     
 _cached_fonts = {}
 def get_font(font_preferences, size):
+    global _cached_text
     key = str(font_preferences) + '|' + str(size)
     font = _cached_fonts.get(key, None)
     if font == None:
         font = make_font(font_preferences, size)
-        global _cached_fonts
         _cached_fonts[key] = font
     return font
 
 _cached_text = {}
 def create_text(text, fonts, size, color):
+    global _cached_text
     key = '|'.join(map(str, (fonts, size, color, text)))
     image = _cached_text.get(key, None)
     if image == None:
         font = get_font(fonts, size)
         image = font.render(text, True, color)
-        global _cached_text
         _cached_text[key] = image
     return image
 
@@ -312,7 +312,7 @@ class rect(pygame.Rect):
             pygame.draw.rect(gameDisplay, self.color, self)
 
     def destroy(self) -> None:
-        spriteList.remove(self)
+        shapeList.remove(self)
         del (self)
 
     def collide(self, otherRect) -> bool:
